c0c56c84c0c4710b6c1bbff1be9171278ce4e35e
Seo <seohyun6361@gmail.com>
2025-10-28 05:40:33 +0900

    feat: 카라비너 선택 뷰, 키링 추가 뷰 추가

--- Keychy/Keychy/Features/Collection/Views/Bundle/BundleSelectCarabinerView.swift
+++ Keychy/Keychy/Features/Collection/Views/Bundle/BundleSelectCarabinerView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct BundleSelectCarabinerView: View {
+    @Bindable var router: NavigationRouter<HomeRoute>
+    
+    @State var viewModel: CollectionViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("카라비너 선택 화면입니다.")
+        Text("선택한 배경화면 이름 : \(viewModel.selectedBackground.backgroundName)")
     }
 }
 
 #Preview {
-    BundleSelectCarabinerView()
+    BundleSelectCarabinerView(router: NavigationRouter(), viewModel: CollectionViewModel())
 }
