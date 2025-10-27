340af6b295981c2469a89700ac71dbb43aca3a01
Seo <seohyun6361@gmail.com>
2025-10-28 05:33:48 +0900

    feat: 카라비너 선택 뷰 추가

---
+++ Keychy/Keychy/Features/Collection/Views/Bundle/BundleSelectCarabinerView.swift
@@ -0,0 +1,18 @@
+//
+//  BundleSelectCarabinerView.swift
+//  Keychy
+//
+//  Created by 김서현 on 10/28/25.
+//
+
+import SwiftUI
+
+struct BundleSelectCarabinerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BundleSelectCarabinerView()
+}
