097aea84459feedc9d8a4cfa88c6b8328c0f5eeb
Jini <yjkwon424@gmail.com>
2025-10-28 06:52:31 +0900

    chore: 앱 진입 임시 뷰 생성

---
+++ Keychy/Keychy/Features/Intro/Views/SplashView.swift
@@ -0,0 +1,15 @@
+//
+//  SplashView.swift
+//  Keychy
+//
+//  Created by Jini on 10/27/25.
+//
+
+import SwiftUI
+
+// 스플래쉬 뷰
+struct SplashView: View {
+    var body: some View {
+        Text("SplashView")
+    }
+}
