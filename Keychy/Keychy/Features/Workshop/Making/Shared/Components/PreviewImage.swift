49b18b3d87f7d88ee23ce2ea387be8c3473f2c87
giljihun <accc45@outlook.com>
2025-10-28 23:03:40 +0900

    feat: 키링프리뷰 컴포넌트 생성

---
+++ Keychy/Keychy/Features/Workshop/Making/Shared/Components/PreviewImage.swift
@@ -0,0 +1,26 @@
+//
+//  TemplatePreviewImage.swift
+//  Keychy
+//
+//  Created by 길지훈 on 10/28/25.
+//
+
+import SwiftUI
+
+struct PreviewImage: View {
+    
+    /// 파이어 스토어에서 가져올 프리뷰이미지
+    let previewURL: String
+    
+    var body: some View {
+        AsyncImage(url: URL(string: previewURL)) { image in
+            image
+        } placeholder: {
+            ProgressView()
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    PreviewImage(previewURL: "https://firebasestorage.googleapis.com/v0/b/keychy-f6011.firebasestorage.app/o/Templates%2FacrylicPhoto%2FacrylicPreview.png?alt=media&token=cc1e53cf-9de2-4a32-a50f-f02339999f24")
+}
