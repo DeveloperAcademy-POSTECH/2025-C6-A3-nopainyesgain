b79cb3b444572221c1ee2b3f5037a2b4b1c581c0
Seo <seohyun6361@gmail.com>
2025-10-28 16:44:14 +0900

    feat: 모델 수정
    state 필드 삭제

--- Keychy/Keychy/CommonModels/KeyringBundle/Background.swift
+++ Keychy/Keychy/CommonModels/KeyringBundle/Background.swift
@@ -13,8 +13,6 @@ struct Background: Identifiable, Equatable, Hashable {
     var backgroundId: String
     var backgroundImage: String
     var tags: [String]
-    /// state: 무료(0) / 구매 전(1) / 보유 중(2)
-    var state: Int
     var price: Int
     var downloadCount: Int
     var useCount: Int
