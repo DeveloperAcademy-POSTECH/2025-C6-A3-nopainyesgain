6067cd60754e97f5e8999e9f1bc784291824a775
Seo <seohyun6361@gmail.com>
2025-10-29 11:57:52 +0900

    feat: 카라비너 그리드 아이템 타일 컴포넌트 추가

---
+++ Keychy/Keychy/Core/Components/View/KeyringBundle/CarabinerItemTile.swift
@@ -0,0 +1,42 @@
+//
+//  CarabinerSelectItemTile.swift
+//  Keychy
+//
+//  Created by 김서현 on 10/29/25.
+//
+
+import SwiftUI
+
+struct CarabinerItemTile: View {
+    var isSelected: Bool
+    var carabiner: Carabiner
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Image(carabiner.carabinerImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .padding(10)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.gray.opacity(0.1))
+                )
+                .overlay(
+                    isSelected ?
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(.black100.opacity(0.15)) : nil
+                )
+            Image(.cherries)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32, height: 32)
+                .padding(.top, 7)
+                .padding(.leading, 10)
+        }
+    }
+}
+
+#Preview {
+    CarabinerItemTile(isSelected: true, carabiner: Carabiner(carabinerName: "카라비너", carabinerId: "123", carabinerImage: "ddochi", description: "des", maxKeyringCount: 1, tags: ["tags"], price: 1, downloadCount: 1, useCount: 1, createdAt: Date(), keyringXPosition: [], keyringYPosition: []))
+}
