5c69cfb22d97059da5eef682d8f5920df3af273a
서현 Seo <81681967+jokingzz@users.noreply.github.com>
2025-10-29 14:53:44 +0900

    Feat/뭉치 생성 카라비너선택 뷰 제작 (#85)
    * fix: 파일 안 올라감 수정
    
    * feat: 키링 추가하기 뷰 루트 추가
    
    * feat: 카라비너 위에 달리는 +버튼 컴포넌트 추가
    
    * feat: 카라비너 그리드 아이템 타일 컴포넌트 추가
    
    * feat: 뷰모델에 Observable 추가
    
    * feat: 카라비너 선택 화면 UI 작업
    
    hifi 아님
    
    * fix: 프로퍼티 소문자로 변경

---
+++ Keychy/Keychy/Core/Components/View/KeyringBundle/CarabinerAddKeyringButton.swift
@@ -0,0 +1,25 @@
+//
+//  CarabinerAddKeyringButton.swift
+//  Keychy
+//
+//  Created by 김서현 on 10/29/25.
+//
+/// 카라비너에 키링 달릴 위치를 표시하는 + 버튼입니다.
+import SwiftUI
+
+struct CarabinerAddKeyringButton: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 8.73)
+            .fill(.black100)
+            .stroke(.white, lineWidth: 1.5)
+            .frame(width: 24, height: 24)
+            .overlay {
+                Image(systemName: "plus")
+                    .foregroundStyle(.white)
+            }
+    }
+}
+
+#Preview {
+    CarabinerAddKeyringButton()
+}
--- Keychy/Keychy/Features/Collection/Views/Bundle/BundleSelectCarabinerView.swift
+++ Keychy/Keychy/Features/Collection/Views/Bundle/BundleSelectCarabinerView.swift
@@ -2,18 +2,119 @@
 //  BundleSelectCarabinerView.swift
 //  Keychy
 //
-//  Created by 김서현 on 10/28/25.
+//  Created by 김서현 on 10/26/25.
 //
 
 import SwiftUI
 
 struct BundleSelectCarabinerView: View {
     @Bindable var router: NavigationRouter<HomeRoute>
-    
     @State var viewModel: CollectionViewModel
+    
     var body: some View {
-        Text("카라비너 선택 화면입니다.")
-        Text("선택한 배경화면 이름 : \(viewModel.selectedBackground.backgroundName)")
+        GeometryReader { geo in
+            VStack(alignment: .center) {
+                carabinerImageView
+                    .frame(width: geo.size.width * 0.5)
+                Spacer()
+                selectCarabinerbottomSection
+            }
+            .navigationBarBackButtonHidden(true)
+            .ignoresSafeArea(edges: .bottom)
+            .toolbar {
+                backToolbarItem
+                nextToolbarItem
+            }
+            .background(
+                // 배경화면 이미지
+                Image(.cherries)
+                    .resizable()
+                    .blur(radius: 10)
+                    .scaledToFill()
+            )
+        }
+    }
+}
+
+//MARK: - 툴바
+extension BundleSelectCarabinerView {
+    private var backToolbarItem: some ToolbarContent {
+        ToolbarItem(placement: .topBarLeading) {
+            Button(action: {
+                router.pop()
+            }) {
+                Image(systemName: "chevron.left")
+            }
+        }
+    }
+    
+    private var nextToolbarItem: some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            Button("다음") {
+                router.push(.bundleAddKeyringView)
+            }
+        }
+    }
+}
+
+//MARK: - 카라비너 뷰
+extension BundleSelectCarabinerView {
+    private var carabinerImageView: some View {
+        VStack {
+            // 카라비너 이미지
+            Image(.basicRing)
+                .resizable()
+                .scaledToFit()
+                .overlay {
+                    if let carabiner = viewModel.selectedCarabiner {
+                        GeometryReader { geo in
+                            ForEach(0..<carabiner.maxKeyringCount, id: \.self) { index in
+                                CarabinerAddKeyringButton()
+                                    .position(
+                                        x: geo.size.width * carabiner.keyringXPosition[index],
+                                        y: geo.size.height * carabiner.keyringYPosition[index]
+                                    )
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
+
+//MARK: - 하단 카라비너 선택 뷰
+extension BundleSelectCarabinerView {
+    private var selectCarabinerbottomSection: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("카라비너")
+                Spacer()
+            }
+            ScrollView(.horizontal) {
+                HStack(spacing: 10) {
+                    ForEach(viewModel.carabiner, id: \.self) { cb in
+                        Button {
+                            viewModel.selectedCarabiner = cb
+                        } label: {
+                            CarabinerItemTile(isSelected: viewModel.selectedCarabiner == cb, carabiner: cb)
+                        }
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(.top, 25)
+        .padding(.horizontal, 16)
+        .padding(.bottom, 115)
+        .frame(maxWidth: .infinity)
+        .background(
+            UnevenRoundedRectangle(
+                topLeadingRadius: 20,
+                topTrailingRadius: 20
+            )
+            .fill(Color.white.opacity(0.8))
+            .shadow(radius: 10)
+        )
     }
 }
 
