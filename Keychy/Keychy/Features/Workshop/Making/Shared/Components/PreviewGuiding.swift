a315ab785e90ee9a478290f830a0f7bff0775be9
giljihun <accc45@outlook.com>
2025-10-29 22:53:09 +0900

    feat: 템플릿모델 -> 가이드이미지URL, 가이드 텍스트 프로퍼티 추가
    - 제작 플로우 가이딩에 사용될 내용

---
+++ Keychy/Keychy/Features/Workshop/Making/Shared/Components/PreviewGuiding.swift
@@ -0,0 +1,18 @@
+//
+//  KeyringGuiding.swift
+//  Keychy
+//
+//  Created by 길지훈 on 10/29/25.
+//
+
+import SwiftUI
+
+struct KeyringGuiding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyringGuiding()
+}
