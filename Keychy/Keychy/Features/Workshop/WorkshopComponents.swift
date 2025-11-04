e6ad7752e86942b18b318affbc56c0f2fec68de3
rundo <rundo999@gmail.com>
2025-11-04 11:29:50 +0900

    fix: workshopView 썸네일 꽉차게

--- Keychy/Keychy/Features/Workshop/WorkshopComponents.swift
+++ Keychy/Keychy/Features/Workshop/WorkshopComponents.swift
@@ -158,7 +158,6 @@ struct WorkshopItemView<Item: WorkshopItem>: View {
                                 }
                         }
                     }
-                    .scaledToFit()
                 }
             }
             .padding(.vertical,10)
