feeb5e0fe03934a42674e8404f1d6480f681b376
Seo <seohyun6361@gmail.com>
2025-11-08 13:18:43 +0900

    feat: 뭉치 리스트 아이템 수정

--- Keychy/Keychy/Core/Components/View/KeyringBundle/KeyringBundleItem.swift
+++ Keychy/Keychy/Core/Components/View/KeyringBundle/KeyringBundleItem.swift
@@ -13,29 +13,43 @@ struct KeyringBundleItem: View {
     
     var body: some View {
         VStack(spacing: 5) {
-            //TODO: 실제 뭉치 씬으로 변경 필요
-            Image(.ddochi)
-                .resizable()
-                .scaledToFit()
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.gray.opacity(0.2))
-                )
+            ZStack(alignment: .top) {
+                //TODO: 실제 뭉치 씬으로 변경 필요
+                Image(.ddochi)
+                    .resizable()
+                    .scaledToFit()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.gray.opacity(0.2))
+                    )
+                UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10)
+                    .fill(.pink100.opacity(0.7))
+                    .overlay(
+                        Text("대표")
+                            .typography(.suit13M)
+                            .foregroundStyle(.white100)
+                    )
+                    .frame(height: 26)
+                    .frame(maxWidth: .infinity)
+                
+            }
             
             HStack {
-                bundle.isMain ? Image(systemName: "pin.fill") : nil
                 Text(bundle.name)
-                    .font(.system(size: 16, weight: .semibold))
+                    .typography(.suit15SB25)
+                    .foregroundStyle(.black100)
                 Spacer()
             }
             HStack {
                 Text("걸린 키링")
-                    .font(.system(size: 11, weight: .medium))
+                    .typography(.suit12M)
+                    .foregroundStyle(.gray500)
                 Spacer()
                 Text("\(bundle.keyrings.count) / \(bundle.maxKeyrings) 개")
-                    .foregroundStyle(Color.blue)
-                    .font(.system(size: 13, weight: .medium))
+                    .typography(.suit12M)
+                    .foregroundStyle(.main500)
             }
         }
+        
     }
 }
