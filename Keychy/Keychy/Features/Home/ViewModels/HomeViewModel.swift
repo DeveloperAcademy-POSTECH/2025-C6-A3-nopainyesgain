33d7a3d59b4f45f1932d21d87defeeba97ce073c
Seo <seohyun6361@gmail.com>
2025-11-08 20:10:10 +0900

    feat: HomeViewModel 번들 관련 메서드 추가

--- Keychy/Keychy/Features/Home/ViewModels/HomeViewModel.swift
+++ Keychy/Keychy/Features/Home/ViewModels/HomeViewModel.swift
@@ -10,6 +10,18 @@ import Foundation
 
 @Observable
 class HomeViewModel {
+    // MARK: - Collection ViewModel
+    var collectionViewModel: CollectionViewModel
     
+    // MARK: - Bundle Scene States
+    var didPrefetchBundle: Bool = false
+    var isBundleLoading: Bool = false
+    var isBundleSceneReady: Bool = false
+    var bundleScenePreparationDelay: Bool = false
+    var allBundleKeyringsStabilized: Bool = false
+    
+    init(collectionViewModel: CollectionViewModel = CollectionViewModel()) {
+        self.collectionViewModel = collectionViewModel
+    }
 }
 
