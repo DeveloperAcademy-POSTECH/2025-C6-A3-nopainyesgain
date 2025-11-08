929c0b6a95563e53c28c07c787f02239a32eed4d
Seo <seohyun6361@gmail.com>
2025-11-08 20:21:54 +0900

    feat: HomeViewModel 번들 관련 로직 삭제

--- Keychy/Keychy/Features/Home/ViewModels/HomeViewModel.swift
+++ Keychy/Keychy/Features/Home/ViewModels/HomeViewModel.swift
@@ -13,13 +13,6 @@ class HomeViewModel {
     // MARK: - Collection ViewModel
     var collectionViewModel: CollectionViewModel
     
-    // MARK: - Bundle Scene States
-    var didPrefetchBundle: Bool = false
-    var isBundleLoading: Bool = false
-    var isBundleSceneReady: Bool = false
-    var bundleScenePreparationDelay: Bool = false
-    var allBundleKeyringsStabilized: Bool = false
-    
     init(collectionViewModel: CollectionViewModel = CollectionViewModel()) {
         self.collectionViewModel = collectionViewModel
     }
