5e6015a791e4d6ca6435c8e3b98119ae7a2d2ac9
giljihun <accc45@outlook.com>
2025-11-09 02:54:26 +0900

    style: 알림뷰 - 알림 on/off 뷰

--- Keychy/Keychy/Features/Home/Views/AlarmView.swift
+++ Keychy/Keychy/Features/Home/Views/AlarmView.swift
@@ -8,7 +8,69 @@
 import SwiftUI
 
 struct AlarmView: View {
+    
+    @State private var isNotiEmpty: Bool = false
+    @State private var isNotiOff: Bool = true
+    @State private var isNotiOffShown: Bool = false
+    
     var body: some View {
-        Text("알람창뷰 입니다.")
+        ZStack {
+            emptyImageView
+            VStack(spacing: 0) {
+                if isNotiOff {
+                    pushNotiOffView
+                }
+                
+                Spacer()
+                   
+            }
+        }
+        .padding(.top, 10)
+        .navigationTitle("알림")
+        .navigationBarTitleDisplayMode(.inline)
+        
+    }
+    
+}
+
+
+extension AlarmView {
+    private var emptyImageView: some View {
+        VStack(alignment: .center, spacing: 0) {
+            Image("EmptyViewIcon")
+            Text("알림함이 비었어요.")
+                .typography(.suit15R)
+                .padding(15)
+        }
+    }
+    
+    private var pushNotiOffView: some View {
+        HStack {
+            /// 알람 아이콘
+            Image("AlarmIconFill")
+                .padding(.vertical, 3.5)
+                .padding(.trailing, 12)
+            
+            /// 알림 off 텍스트
+            VStack(spacing: 8) {
+                Text("기기 알림이 꺼져있어요! 알림을 켜주세요.")
+                    .typography(.suit15B25)
+                    .foregroundStyle(.black100)
+                Text("눌러서 알림 활성화 하기")
+                    .typography(.suit13M)
+                    .foregroundStyle(.gray400)
+            }
+            .padding(.trailing, 38)
+            
+            /// 알림 off 뷰 닫기 버튼
+            Button {
+                isNotiOffShown = false
+            } label: {
+                Image("dismiss_gray300")
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 18)
+        .background(.gray50)
     }
 }
