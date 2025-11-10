5f1eaf85159d3fa5e450554c402f7a215769cb45
Seo <seohyun6361@gmail.com>
2025-11-10 17:35:46 +0900

    feat: notosans16R 폰트 추가

--- Keychy/Keychy/Core/DesignSystem/Typography/Typography.swift
+++ Keychy/Keychy/Core/DesignSystem/Typography/Typography.swift
@@ -78,6 +78,7 @@ struct Typography {
     // MARK: - NotoSans
     static let notosans15M = Typography(font: .custom(.notoSansMedium, size: 15), lineSpacing: 0)
     static let notosans15R = Typography(font: .custom(.notoSansRegular, size: 15), lineSpacing: 0)
+    static let notosans16R = Typography(font: .custom(.notoSansRegular, size: 16), lineSpacing: 0)
     static let notosans20M = Typography(font: .custom(.notoSansMedium, size: 20), lineSpacing: 0)
     static let notosans24M = Typography(font: .custom(.notoSansMedium, size: 24), lineSpacing: 0)
     
