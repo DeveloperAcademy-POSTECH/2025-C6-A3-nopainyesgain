632066fab3480120976fb3d2d8661a38ad6844d9
Seo <seohyun6361@gmail.com>
2025-11-12 02:49:12 +0900

    feat: 배경 선택 그리드 아이템, 카라비너 선택 그리드 아이템 디자인 수정

--- Keychy/Keychy/Core/Components/View/KeyringBundle/SelectBackgroundGridItem.swift
+++ Keychy/Keychy/Core/Components/View/KeyringBundle/SelectBackgroundGridItem.swift
@@ -13,65 +13,71 @@ struct SelectBackgroundGridItem: View {
     let isSelected: Bool
     
     var body: some View {
-        ZStack {
-            // 배경 이미지
-            LazyImage(url: URL(string: background.background.backgroundImage)) { state in
-                if let image = state.image {
-                    image
-                        .resizable()
-                        .aspectRatio(5/7, contentMode: .fit)
-                } else if state.isLoading {
-                    ProgressView()
-                        .aspectRatio(5/7, contentMode: .fit)
-                }
-            }
-            .scaledToFit()
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(isSelected ? .black.opacity(0.15) : .clear)
-                
-            )
-            VStack {
-                HStack {
-                    // 유료 아이콘
-                    if !background.background.isFree {
-                        Image(.keyHole)
+        VStack(spacing: 10) {
+            ZStack {
+                // 배경 이미지
+                LazyImage(url: URL(string: background.background.backgroundImage)) { state in
+                    if let image = state.image {
+                        image
                             .resizable()
-                            .frame(width: 30, height: 30)
-                            .padding(.top, 3)
+                            .scaledToFill()
+                            .frame(width: 114, height: 152)
+                    } else if state.isLoading {
+                        ProgressView()
+                            .aspectRatio(5/7, contentMode: .fit)
                     }
+                }
+                .scaledToFit()
+                .clipped()
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(isSelected ? .black.opacity(0.15) : .clear)
+                    
+                )
+                VStack {
+                    HStack {
+                        // 유료 아이콘
+                        if !background.background.isFree {
+                            Image(.keyHole)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .padding(.top, 3)
+                        }
+                        
+                        Spacer()
+                        // 보유 표시
+                        if background.isOwned {
+                            Text("보유")
+                                .typography(.suit13M)
+                                .foregroundStyle(.white100)
+                                .padding(.horizontal, 8)
+                                .padding(.vertical, 4)
+                                .background(Color.black.opacity(0.6))
+                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                        }
+                    }
+                    .padding(.horizontal, 5)
+                    .padding(.top, 3)
                     
                     Spacer()
-                    // 보유 표시
-                    if background.isOwned {
-                        Text("보유")
-                            .typography(.suit13M)
-                            .foregroundStyle(.white100)
-                            .padding(.horizontal, 8)
-                            .padding(.vertical, 4)
-                            .background(Color.black.opacity(0.6))
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                    
+                    HStack {
+                        Spacer()
+                        // 다운로드 아이콘
+                        if background.background.isFree && !background.isOwned {
+                            Image(.download)
+                                .resizable()
+                                .frame(width: 18, height: 18)
+                                .padding(7)
+                        }
                     }
                 }
-                .padding(.horizontal, 5)
-                .padding(.top, 3)
-                
-                Spacer()
-                
-                // 다운로드 아이콘
-                if background.background.isFree && !background.isOwned {
-                    Image(.download)
-                        .resizable()
-                        .frame(width: 18, height: 18)
-                        .padding(7)
-                }
             }
-        }
-
-        // 이름 라벨
-        Text(background.background.backgroundName)
-            .typography(isSelected ? .notosans14SB : .notosans14M)
-            .foregroundStyle(isSelected ? .main500 : .black100)
+            // 이름 라벨
+            Text(background.background.backgroundName)
+                .typography(isSelected ? .notosans14SB : .notosans14M)
+                .foregroundStyle(isSelected ? .main500 : .black100)
+        } //: VSTACK
     }
 }
