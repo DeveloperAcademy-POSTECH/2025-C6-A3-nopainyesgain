eefbc657ce03ceb7bce680976d2a7b877cecc045
Seo <seohyun6361@gmail.com>
2025-11-12 15:04:55 +0900

    design: 아이콘 색깔 별 에셋 추가

---
+++ Keychy/Keychy/Core/Components/View/KeyringBundle/CarabinerChangePopup.swift
@@ -0,0 +1,18 @@
+//
+//  CarabinerChangePopup.swift
+//  Keychy
+//
+//  Created by 김서현 on 11/12/25.
+//
+
+import SwiftUI
+
+struct CarabinerChangePopup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarabinerChangePopup()
+}
