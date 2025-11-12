9ab7ecbba89445f4659ef7589553759341f43f6c
giljihun <accc45@outlook.com>
2025-11-12 17:17:04 +0900

    fix: 마이페이지 alert 등장하고 툴바가 가려질때 뷰가 위로 이동하던 오류 해결
    - 툴바자체를 지우지않고 백버튼만 지움

---
+++ Keychy/Keychy/Features/Intro/Views/IntroAppGuidingView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroAppGuidingView.swift
+//  Keychy
+//
+//  Created by 길지훈 on 11/12/25.
+//
+
+import SwiftUI
+
+struct IntroAppGuidingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroAppGuidingView()
+}
