d5ee7bbd6940d773214c151f7a405f4b26629e17
giljihun <accc45@outlook.com>
2025-11-17 05:11:36 +0900

    feat: 크롭뷰에서도 로딩이 걸린다는 요청으로 로딩 추가

--- Keychy/Keychy/Features/Workshop/Making/Templates/AcrylicPhoto/Views/AcrylicPhotoCropView.swift
+++ Keychy/Keychy/Features/Workshop/Making/Templates/AcrylicPhoto/Views/AcrylicPhotoCropView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 struct AcrylicPhotoCropView: View {
     @Bindable var router: NavigationRouter<WorkshopRoute>
     @Bindable var viewModel: AcrylicPhotoVM
-    
+
+    @State private var isImageLoading = true
+
     var body: some View {
         ZStack {
             // 배경색
@@ -18,36 +20,39 @@ struct AcrylicPhotoCropView: View {
                             .resizable()
                             .scaledToFit()
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .opacity(isImageLoading ? 0 : 1)
                             .onAppear {
-                                viewModel.imageViewSize = geo.size
-                                viewModel.fixedImage = viewModel.selectedImage?.fixedOrientation()
-                                if !viewModel.hasCropAreaBeenSet {
-                                    viewModel.resetToCenter()
-                                }
+                                setupImageAndCropArea(containerSize: geo.size)
                             }
                             .onChange(of: geo.size) { _, newSize in
                                 viewModel.imageViewSize = newSize
-                                // 무조건 다시 계산 (처음 설정이 잘못된 거 보정)
                                 viewModel.resetToCenter()
                             }
                     }
 
                     // 크롭박스 바깥 영역 어둡게 (이미지 영역 내에서만)
-                    DimmingOverlay(
-                        cropRect: viewModel.cropArea,
-                        imageRect: viewModel.getDisplayedImageRect()
-                    )
+                    if !isImageLoading {
+                        DimmingOverlay(
+                            cropRect: viewModel.cropArea,
+                            imageRect: viewModel.getDisplayedImageRect()
+                        )
 
-                    // 크롭박스
-                    CropBoxView(
-                        rect: $viewModel.cropArea,
-                        onDragChanged: viewModel.onDragChanged,
-                        onDragEnd: viewModel.onDragEnd
-                    )
+                        // 크롭박스
+                        CropBoxView(
+                            rect: $viewModel.cropArea,
+                            onDragChanged: viewModel.onDragChanged,
+                            onDragEnd: viewModel.onDragEnd
+                        )
+                    }
                 }
             }
             .padding(.horizontal, 16)
             .padding(.bottom, 70)
+
+            // 로딩 인디케이터
+            if isImageLoading {
+                LoadingAlert(type: .short, message: nil)
+            }
         }
         .navigationBarBackButtonHidden(true)
         .navigationBarTitleDisplayMode(.inline)
@@ -56,6 +61,20 @@ struct AcrylicPhotoCropView: View {
             backToolbarItem
             naivgationTitle
             nextToolbarItem
+        }
+    }
+
+    // MARK: - Setup Methods
+    private func setupImageAndCropArea(containerSize: CGSize) {
+        viewModel.imageViewSize = containerSize
+        viewModel.fixedImage = viewModel.selectedImage?.fixedOrientation()
+
+        if !viewModel.hasCropAreaBeenSet {
+            viewModel.resetToCenter()
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            isImageLoading = false
         }
     }
 }
