60b8fa88afd0186756088205f998eb4ef550524e
giljihun <accc45@outlook.com>
2025-11-16 20:03:07 +0900

    feat: 상단패딩도 추가

--- Keychy/Keychy/Core/Extensions/View+SafeAreaBottom.swift
+++ Keychy/Keychy/Core/Extensions/View+SafeAreaBottom.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 extension View {
-    /// 직각형 기기(SE2, 3? 더 있나)에서만 하단 패딩 추가
-    /// 둥근 모서리 기기는 iOS가 자동으로 패딩 적용해서 추가안함.
+    // 직각형 기기(SE2, 3? 더 있나)에서만 패딩 추가합니다.
+    
+    /// 하단
     func adaptiveBottomPadding(_ defaultPadding: CGFloat = 34) -> some View {
         self.padding(.bottom, getBottomPadding(defaultPadding))
     }
-
+    
+    /// 상단
+    func adaptiveTopPadding(_ defaultPadding: CGFloat = 20) -> some View {
+        self.padding(.top, getTopPadding(defaultPadding))
+    }
+    
+    /// 하단 safeArea 값 반환
     private func getBottomPadding(_ defaultPadding: CGFloat) -> CGFloat {
         guard let window = UIApplication.shared.connectedScenes
             .compactMap({ $0 as? UIWindowScene })
@@ -21,8 +28,20 @@ extension View {
             .first(where: { $0.isKeyWindow }) else {
             return defaultPadding
         }
-
+        
         // safeAreaInsets.bottom이 0이면 직각형 기기임!
         return window.safeAreaInsets.bottom == 0 ? defaultPadding : 0
     }
+    /// 상단 safeArea 값 반환
+    private func getTopPadding(_ defaultPadding: CGFloat) -> CGFloat {
+        guard let window = UIApplication.shared.connectedScenes
+            .compactMap({ $0 as? UIWindowScene })
+            .first?.windows
+            .first(where: { $0.isKeyWindow }) else {
+            return defaultPadding
+        }
+        
+        return window.safeAreaInsets.top == 0 ? defaultPadding : 0
+    }
+    
 }
