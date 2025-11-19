2a209a7b250016d5f838dcaf2ac2e0d1d60bac1b
서현 Seo <seohyun6361@gmail.com>
2025-11-19 14:03:02 +0900

    fix: 뭉치 보관함 상단 패딩값 추가

--- Keychy/Keychy/Features/Collection/Views/Bundle/BundleInventoryView.swift
+++ Keychy/Keychy/Features/Collection/Views/Bundle/BundleInventoryView.swift
@@ -24,12 +24,7 @@ struct BundleInventoryView<Route: BundleRoute>: View {
     
     var body: some View {
         ZStack(alignment: .top) {
-            VStack {
-                bundleGrid()
-            }
-            .padding(.top, 20)
-            .adaptiveTopPaddingAlt()
-            .padding(.horizontal, 20)
+            bundleGrid()
             
             customNavigationBar
         }
@@ -105,25 +100,35 @@ extension BundleInventoryView {
 // MARK: - 그리드 뷰
 extension BundleInventoryView {
     private func bundleGrid() -> some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 18) {
-                ForEach(viewModel.sortedBundles, id: \.self) { bundle in
-                    Button {
-                        // 선택한 번들 설정
-                        viewModel.selectedBundle = bundle
-                        // 번들에 저장된 id(String)를 실제 모델로 해석하여 선택 상태에 반영
-                        viewModel.selectedBackground = viewModel.resolveBackground(from: bundle.selectedBackground)
-                        viewModel.selectedCarabiner = viewModel.resolveCarabiner(from: bundle.selectedCarabiner)
-                        
-                        // 상세 화면으로 이동
-                        isNavigatingDeeper = true
-                        router.push(.bundleDetailView)
-                    } label: {
-                        KeyringBundleItem(bundle: bundle)
+        // 동그라미 기기라면 추가적인 패딩값을 줍니다
+        var morePadding: CGFloat = 0
+        if getBottomPadding(34) == 0 {
+            morePadding = 40
+        }
+        return GeometryReader { geometry in
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 18) {
+                    ForEach(viewModel.sortedBundles, id: \.self) { bundle in
+                        Button {
+                            // 선택한 번들 설정
+                            viewModel.selectedBundle = bundle
+                            // 번들에 저장된 id(String)를 실제 모델로 해석하여 선택 상태에 반영
+                            viewModel.selectedBackground = viewModel.resolveBackground(from: bundle.selectedBackground)
+                            viewModel.selectedCarabiner = viewModel.resolveCarabiner(from: bundle.selectedCarabiner)
+                            
+                            // 상세 화면으로 이동
+                            isNavigatingDeeper = true
+                            router.push(.bundleDetailView)
+                        } label: {
+                            KeyringBundleItem(bundle: bundle)
+                        }
                     }
                 }
+                .padding(.horizontal, 20)
+                .padding(.top, geometry.safeAreaInsets.top + 60 + 20 + morePadding) // safe area + 네비 바 높이 + 여백
             }
         }
+        
     }
 }
 
