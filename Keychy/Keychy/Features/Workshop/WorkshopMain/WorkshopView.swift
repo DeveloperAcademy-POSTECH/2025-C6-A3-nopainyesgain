d42f8eaf27225c0175480c0a1f1b17ad37ffd5c9
rundo <rundo999@gmail.com>
2025-11-24 07:23:20 +0900

    fix: 배경 이름수정

--- Keychy/Keychy/Features/Workshop/WorkshopMain/WorkshopView.swift
+++ Keychy/Keychy/Features/Workshop/WorkshopMain/WorkshopView.swift
@@ -55,7 +55,7 @@ struct WorkshopView: View {
             .allowsHitTesting(false)
         }
         .background(
-            Image("Back")
+            Image(.back)
                 .resizable()
                 .scaledToFill()
         )
