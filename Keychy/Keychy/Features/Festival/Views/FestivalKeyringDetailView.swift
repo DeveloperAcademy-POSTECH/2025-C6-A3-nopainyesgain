4a8f35b03014c0c647a2a338ea167d045d885541
서현 Seo <seohyun6361@gmail.com>
2025-11-23 21:14:36 +0900

    feat: selectedIndex로 키링 정보 가져오기

--- Keychy/Keychy/Features/Festival/Views/FestivalKeyringDetailView.swift
+++ Keychy/Keychy/Features/Festival/Views/FestivalKeyringDetailView.swift
@@ -10,32 +10,40 @@ import NukeUI
 
 struct FestivalKeyringDetailView: View {
     @Bindable var router: NavigationRouter<WorkshopRoute>
-    @Bindable var viewModel: FestivalViewModel
+    @Bindable var viewModel: Showcase25BoardViewModel
     
     @State var authorName: String = ""
+    @State var keyring: Keyring?
     
     @State var showVoteAlert: Bool = false
     
-    let keyring: Keyring
     // keyringDetailView에 이거 있길래 데려왔는데 필요 없으면 지우세욥!!
     let heightRatio: CGFloat = screenHeight / 852
     // geometryReader 안 써도 screenWidth, screenHeight 전역으로 선언해둿어용 쓰시면 돼용
     var body: some View {
         ZStack {
             VStack {
-                // 정보 잘 나오는지 확인하는 용으로 해둔 임의 디자인이에용 수정하시면 됩니닷
-                LazyImage(url: URL(string: keyring.bodyImage)) { state in
-                    if let image = state.image {
-                        image
-                            .resizable()
-                            .frame(height: screenHeight * 0.3)
+                if let keyring = keyring {
+                    // 정보 잘 나오는지 확인하는 용으로 해둔 임의 디자인이에용 수정하시면 됩니닷
+                    LazyImage(url: URL(string: keyring.bodyImage)) { state in
+                        if let image = state.image {
+                            image
+                                .resizable()
+                                .frame(height: screenHeight * 0.3)
+                        }
                     }
+                    Text("키링 제출자 : \(authorName)")
+                } else {
+                    ProgressView()
                 }
-                Text("키링 제출자 : \(authorName)")
             }
             .padding(.horizontal, 80)
             
             customNavigationBar
+        }
+        .task {
+            // 뷰가 나타날 때 선택된 키링 로드
+            keyring = await viewModel.getSelectedKeyring()
         }
     }
 }
@@ -48,7 +56,7 @@ extension FestivalKeyringDetailView {
                 router.pop()
             }
         } center: {
-            Text(keyring.name)
+            Text(keyring?.name ?? "키링 상세")
         } trailing: {
             
         }
