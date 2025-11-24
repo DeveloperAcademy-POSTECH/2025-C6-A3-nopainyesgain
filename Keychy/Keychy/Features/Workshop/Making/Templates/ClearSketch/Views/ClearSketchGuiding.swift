8a8544df0a7ba0e67886164ed6aad0fcb2d24048
Riel <96573160+jini-coding@users.noreply.github.com>
2025-11-24 10:28:17 +0900

    feat, style, fix: 클리어 스케치 템플릿 완성 및 하이파이 거의 적용, 키링 편집 완료 시 보관함으로 돌아갈 때 탭바 안 뜨는 현상 해결 (#345)
    * feat: 드로잉 하이파이
    
    * style: 아이콘 디자인 살짝 수정
    
    * style: 슬라이더 위치 조정
    
    * feat: 뒤로가기 시 캔버스 초기화
    
    * chore: 클리어 스케치 크롭뷰 임시 추가
    
    * style: 팔레트 영역 스타일 조정
    
    * style: 화면 대응 UI 조정
    
    * feat: 스케치 이미지 처리 추가
    
    * fix: 이미지 캡쳐 방식으로 변경
    
    * feat: 뒤로가기 시 초기화 alert 추가
    
    * feat: 베지어 곡선으로 변경
    
    * feat: 크롭 path도 베지어 곡선으로 변경
    
    * chore: 크롭 관련 코드 이사용 파일 임시 생성
    
    * chore: 필요없는 주석, print문 제거
    
    * chore: 캔버스 영역 확인용 border 주석 처리
    
    * style: 버튼 디자인 수정
    
    * fix: 키링 수정 완료 시 보관함으로 돌아갈 때 탭바 안 뜨는 현상 픽스
    
    * fix: 드로잉 시 점은 캡쳐되지 않는 현상 픽스
    
    * feat: 크롭 path에 점 하나만 있을 때 다음 단계 진입을 막고 alert 표시 추가
    
    * feat: 가이딩 시트 추가
    
    * fix: 태그 삭제 시 팝업 중첩 해결
    
    * style: 크롭뷰 시 근처 영역 배경색 gray50
    
    * fix: 지우개로 지우면 투명 대신 하얀색으로 설정
    
    * chore: progressview를 LoadingAlert로 변경
    
    * feat: 스케치 템플릿 커스터마이징뷰에서 뒤로가기 시 크롭전 이미지 유지
    
    * fix: 픽셀키링도 뒤로가기 시 팝만 가능하도록 함

---
+++ Keychy/Keychy/Features/Workshop/Making/Templates/ClearSketch/Views/ClearSketchGuiding.swift
@@ -0,0 +1,75 @@
+//
+//  ClearSketchGuiding.swift
+//  Keychy
+//
+//  Created by Jini on 11/24/25.
+//
+
+import SwiftUI
+import NukeUI
+
+struct ClearSketchGuiding: View {
+    @Environment(\.dismiss) var dismiss
+    let guidingText: String
+    let guidingImageURL: String
+
+    @State private var contentHeight = screenHeight * 0.5
+
+    var body: some View {
+        VStack(spacing: 0) {
+            // 상단 닫기 버튼
+            HStack {
+                Button {
+                    dismiss()
+                } label: {
+                    Image("dismiss_gray600")
+                }
+                .padding(.top, 30)
+                .padding(.leading, 20)
+                Spacer()
+            }
+            .padding(.bottom, 19.5)
+            
+            guidingTextLabel
+                .padding(.bottom, 28)
+            
+            guidingImage
+                .padding(.bottom, 23)
+                .adaptiveBottomPadding()
+        }
+        .background(
+            GeometryReader { geometry in
+                Color.clear.preference(
+                    key: GuidingHeightPreferenceKey.self,
+                    value: geometry.size.height
+                )
+            }
+        )
+        .background(Color.white100) // VStack 전체 배경
+        .presentationBackground(Color.white100) // 시트 배경 (위로 땡겨도 하얀색)
+        .onPreferenceChange(GuidingHeightPreferenceKey.self) { height in
+            if height > 0 {
+                contentHeight = height
+            }
+        }
+        .presentationDetents([.height(contentHeight)])
+    }
+}
+
+// MARK: - Components
+extension ClearSketchGuiding {
+    private var guidingTextLabel: some View {
+        Text(guidingText)
+            .typography(.suit20B)
+            .multilineTextAlignment(.center)
+            .frame(maxWidth: .infinity)
+    }
+    
+    private var guidingImage: some View {
+        Image("acrylicGudingImage")
+            .resizable()
+            .scaledToFit()
+            .frame(minHeight: 272.87)
+            .padding(.horizontal, 30)
+    }
+}
