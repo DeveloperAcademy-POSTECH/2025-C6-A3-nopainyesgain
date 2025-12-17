8ce14495bc219855e402a00f0e32822818257ca3
giljihun <accc45@outlook.com>
2025-12-17 18:39:22 +0900

    feat: HomeView 진입 시, 공방 배너 미리 로드

--- Keychy/Keychy/Features/Home/Views/Main/HomeView.swift
+++ Keychy/Keychy/Features/Home/Views/Main/HomeView.swift
@@ -81,6 +81,9 @@ struct HomeView: View {
         .task {
             // 최초 뷰가 나타날 때 메인 뭉치 데이터 로드
             await viewModel.loadMainBundle(collectionViewModel: collectionViewModel, onBackgroundLoaded: onBackgroundLoaded)
+
+            // Workshop 배너 미리 로드 (prefetch)
+            await WorkshopDataManager.shared.fetchWorkshopBanner()
         }
         .onChange(of: viewModel.keyringDataList) { _, _ in
             // 키링 데이터가 변경되면 씬 준비 상태 초기화
