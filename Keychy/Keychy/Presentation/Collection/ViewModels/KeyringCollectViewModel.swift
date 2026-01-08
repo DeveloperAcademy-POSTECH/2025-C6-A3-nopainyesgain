57109d5d261e82a40449334e2d313f2cdd0ee9ba
Jini <yjkwon424@gmail.com>
2026-01-08 14:49:41 +0900

    chore: 뷰모델 파일 생성

---
+++ Keychy/Keychy/Presentation/Collection/ViewModels/KeyringCollectViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  KeyringCollectViewModel.swift
+//  Keychy
+//
+//  Created by Jini on 1/8/26.
+//
+
+import SwiftUI
+
+@Observable
+class KeyringCollectViewModel {
+    
+}
---
+++ Keychy/Keychy/Presentation/Collection/ViewModels/KeyringReceiveViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  KeyringReceiveViewModel.swift
+//  Keychy
+//
+//  Created by Jini on 1/8/26.
+//
+
+import SwiftUI
+
+@Observable
+class KeyringReceiveViewModel {
+    
+}
