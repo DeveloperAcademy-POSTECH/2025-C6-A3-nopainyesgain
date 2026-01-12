2b4dad34000c1c350e3ccd625ca61560304f9b9e
Seo <seohyun6361@gmail.com>
2026-01-12 14:10:17 +0900

    refact: 미사용 Foundation import 삭제

--- Keychy/Keychy/Presentation/Bundle/ViewModels/BundleViewModel.swift
+++ Keychy/Keychy/Presentation/Bundle/ViewModels/BundleViewModel.swift
@@ -5,11 +5,10 @@
 //  Created by 김서현 on 1/9/26.
 //
 
-import Foundation
+import SwiftUI
 import FirebaseFirestore
 import FirebaseStorage
 import NukeUI
-import SwiftUI
 
 // MARK: - 화면 표시용 구조체
 struct BackgroundViewData: Identifiable, Equatable, Hashable {
