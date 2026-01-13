b66e945470d744906efc466d8ec832308ba9f07a
giljihun <accc45@outlook.com>
2026-01-13 22:27:01 +0900

    refactor: Sheet 뷰 불필요한 .toolbar(.hidden) 제거

--- Keychy/Keychy/Presentation/Festival/Views/FestivalKeyringDetailView+Sheet.swift
+++ Keychy/Keychy/Presentation/Festival/Views/FestivalKeyringDetailView+Sheet.swift
@@ -38,7 +38,6 @@ extension FestivalKeyringDetailView {
             }
             .scrollDisabled(true)
         }
-        .toolbar(.hidden, for: .tabBar)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.white100)
         .shadow(
